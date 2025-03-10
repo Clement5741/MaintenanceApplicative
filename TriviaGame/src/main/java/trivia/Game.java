71b6472988050f4bfce0be58f089eab2d2ab0e57
Clement5741 <clement.chauvel1@etu.univ-lorraine.fr>
2025-03-10 16:51:51 +0100

    Trivia - Refactored! CHAUVEL

--- TriviaGame/src/main/java/trivia/Game.java
+++ TriviaGame/src/main/java/trivia/Game.java
@@ -6,26 +6,12 @@ import java.util.List;
 import java.util.Set;
 
 public class Game implements IGame {
-   private static final int MAX_PLAYERS = 6;
    private final List<Player> players = new ArrayList<>();
-   private final Set<String> playerNames = new HashSet<>();
    private final QuestionDeck questionDeck = new QuestionDeck();
    private int currentPlayerIndex = 0;
-   private boolean gameStarted = false;
 
    public boolean add(String playerName) {
-      if (gameStarted) {
-         System.out.println("Cannot add new players after game has started.");
-         return false;
-      }
-      if (players.size() >= MAX_PLAYERS) {
-         System.out.println("Maximum number of players reached.");
-         return false;
-      }
-      if (!playerNames.add(playerName)) {
-         System.out.println("Player name must be unique.");
-         return false;
-      }
+
       players.add(new Player(playerName));
       System.out.println(playerName + " was added");
       System.out.println("They are player number " + players.size());
@@ -37,14 +23,13 @@ public class Game implements IGame {
          System.out.println("At least two players are required to start the game.");
          return;
       }
-      gameStarted = true;
       Player currentPlayer = players.get(currentPlayerIndex);
 
       System.out.println(currentPlayer.getName() + " is the current player");
       System.out.println("They have rolled a " + roll);
 
       if (currentPlayer.isInPenaltyBox()) {
-         if (roll % 2 != 0) { // Seul un nombre impair peut sortir de la prison
+         if (roll % 2 != 0) {
             System.out.println(currentPlayer.getName() + " is getting out of the penalty box");
             currentPlayer.getOutOfPenaltyBox();
             currentPlayer.move(roll);
--- TriviaGame/src/main/java/trivia/QuestionDeck.java
+++ TriviaGame/src/main/java/trivia/QuestionDeck.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class QuestionDeck {
     private final Map<String, LinkedList<String>> questionMap = new HashMap<>();
-    private static final String[] CATEGORIES = {"Pop", "Science", "Sports", "Rock", "Geography"};
+    private static final String[] CATEGORIES = {"Pop", "Science", "Sports", "Rock"};
 
     public QuestionDeck() {
         for (String category : CATEGORIES) {
@@ -27,8 +27,7 @@ public class QuestionDeck {
             case 0: case 4: case 8: return "Pop";
             case 1: case 5: case 9: return "Science";
             case 2: case 6: case 10: return "Sports";
-            case 3: case 7: case 11: return "Rock";
-            default: return "Unknown";
+            default: return "Rock";
         }
     }
 }
